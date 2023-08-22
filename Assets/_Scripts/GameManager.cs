8d37ad8789f90fab50fcfca82f9de345ff214899
Chantal Burkhard <chante11@t-online.de>
2023-08-22 22:04:36 +0200

    fixed input field

--- Assets/_Scripts/GameManager.cs
+++ Assets/_Scripts/GameManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,6 +12,9 @@ public class GameManager : MonoBehaviour
     public GameObject mainMenu;
     public GameObject hostMenu;
     public GameObject connectMenu;
+
+    public GameObject serverPrefab;
+    public GameObject clientPrefab;
 
     void Start()
     {
@@ -26,13 +32,41 @@ public class GameManager : MonoBehaviour
 
     public void HostButton()
     {
+        try
+        {
+            Server s = Instantiate(serverPrefab).GetComponent<Server>();
+            s.Init();
+
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.ConnectToServer("127.0.0.1", 6321);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
+        
         mainMenu.SetActive(false);
         hostMenu.SetActive(true);
     }
 
     public void ConnectToServerButton()
     {
+        string hostAdress = GameObject.Find("HostInput").GetComponent<TMP_InputField>().text;
+        Debug.Log(hostAdress);
+        // if no value is given in the text field the host address is set to local host
+        if (hostAdress == "")
+            hostAdress = "127.0.0.1";
 
+        try
+        {
+            Client c = Instantiate(clientPrefab).GetComponent<Client>();
+            c.ConnectToServer(hostAdress, 6321);
+            connectMenu.SetActive(false);
+        }
+        catch (Exception e)
+        {
+            Debug.Log(e.Message);
+        }
     }
 
     public void BackButton()
